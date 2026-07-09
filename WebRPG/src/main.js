7c66e5d9fb269b147c61506517eb59acb9755aa9
Ktsu0 <gabrielwag971@gmail.com>
2026-07-09 19:22:14 -0300

    feat: bootstrap do WebRPG com tela inicial mínima

---
+++ WebRPG/src/main.js
@@ -0,0 +1,10 @@
+import "./estilos/variaveis.css";
+import "./estilos/base.css";
+import "./estilos/paineis.css";
+import { inicializarRoteador, registrarTela, mostrarTela } from "./rotas/roteador.js";
+import { montarTelaInicial } from "./telas/inicial.js";
+
+const app = document.getElementById("app");
+inicializarRoteador(app);
+registrarTela("inicial", montarTelaInicial);
+mostrarTela("inicial");
---
+++ WebRPG/src/telas/inicial.js
@@ -0,0 +1,8 @@
+export function montarTelaInicial(container) {
+  container.innerHTML = `
+    <div class="painel" style="margin: auto; text-align: center;">
+      <h1>THE LOST WORLD</h1>
+      <p>Fundação do WebRPG pronta. Fase 1 (Batalha) começa aqui.</p>
+    </div>
+  `;
+}
